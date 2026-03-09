cfddeb48c6a3cb8c92b08bc66662fcef8bfd9b9e
Okan Bilal Balcı <obbalci@gmail.com>
2026-03-09 13:29:43 +0300

    refactor: simplify header component by removing unused code and optimizing logo rendering

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,9 @@
 
 import React from "react";
 import Link from "next/link";
-import Leaf from "./leaf";
 import Image from "next/image";
-import { useBrowserDetection } from "@/hooks/useBrowserDetection";
 
 export default function Header() {
-  const { isChrome } = useBrowserDetection();
 
   return (
     <header className="mb-8 max-w-7xl mx-auto px-4">
@@ -15,30 +12,13 @@ export default function Header() {
         {/* Logo and Brand */}
         <Link href="/" className="flex items-center gap-3">
           <div className="relative flex items-center justify-center">
-            {isChrome ? (
-              <>
-                <Image
-                  src="/logo4.png"
-                  alt="Jinzo Logo"
-                  width={480}
-                  height={480}
-                  className="object-contain h-12 w-12 opacity-0"
-                />
-                <span className="absolute inset-0 flex items-center justify-center 
-                font-palette-altfour
-                font-bold text-[40px] font-handwriting">
-                  J
-                </span>
-              </>
-            ) : (
-              <Image
+                 <Image
                 src="/icon.png"
                 alt="Jinzo Logo"
-                width={120}
-                height={120}
+                width={64}
+                height={64}
                 className="object-contain h-16 w-auto"
               />
-            )}
           </div>
 
         </Link>
